c8dee0709837c573c96ebf7aec7d2646d3a29bc0
Francesco Doria <doria5@live.it>
2026-07-12 17:46:18 +0300

    typo

--- src/main/java/httpserver/config/ConfigurationManager.java
+++ src/main/java/httpserver/config/ConfigurationManager.java
@@ -3,7 +3,7 @@ package httpserver.config;
 // We're using a Singleton because we don't need more than one configuration manager.
 // We just need one, which is going to be shared across the project.
 public class ConfigurationManager {
-    // This is the instance that we're goint to be using.
+    // This is the instance that we're going to be using.
     private static ConfigurationManager myConfigurationManager;
     private static Configuration myCurrentConfiguration;
 
